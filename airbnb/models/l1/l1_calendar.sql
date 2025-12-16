c62e833c44fa017fdd347ed5b3fb37eb0c0b7203
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-12-16 13:02:47 +0000

    switched listings and calendars

--- airbnb/models/l1/l1_calendar.sql
+++ airbnb/models/l1/l1_calendar.sql
@@ -2,33 +2,32 @@ SELECT
   'amsterdam' AS city,
   DATE '2025-09-11' AS snapshot_date,
   *
-FROM {{ source('l0', 'raw__amsterdam__listings__20250911') }}
-
-UNION ALL
-SELECT
-  'barcelona',
-  DATE '2025-09-14',
-  *
-FROM {{ source('l0', 'raw__barcelona__listings__20250914') }}
+FROM {{ source('l0', 'raw__amsterdam__calendar__20250911') }}
 
 UNION ALL
 SELECT
   'paris',
   DATE '2025-09-12',
   *
-FROM {{ source('l0', 'raw__paris__listings__20250912') }}
+FROM {{ source('l0', 'raw__paris__calendar__20250912') }}
 
 UNION ALL
 SELECT
-  'lisbon',
-  DATE '2025-09-21',
+  'barcelona',
+  DATE '2025-09-14',
   *
-FROM {{ source('l0', 'raw__lisbon__listings__20250921') }}
+FROM {{ source('l0', 'raw__barcelona__calendar__20250914') }}
 
 UNION ALL
 SELECT
   'london',
   DATE '2025-09-14',
   *
-FROM {{ source('l0', 'raw__london__listings__20250914') }}
-;
+FROM {{ source('l0', 'raw__london__calendar__20250914') }}
+
+UNION ALL
+SELECT
+  'lisbon',
+  DATE '2025-09-21',
+  *
+FROM {{ source('l0', 'raw__lisbon__calendar__20250921') }}
