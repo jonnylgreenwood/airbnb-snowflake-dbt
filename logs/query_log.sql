9e2ef6af5e0fb07f42b3767cc6c1f80b43374cd4
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-12-16 12:10:34 +0000

    removed dbt demo files

--- logs/query_log.sql
+++
@@ -1,9 +0,0 @@
--- created_at: 2025-12-16T11:28:32.396663+00:00
--- finished_at: 2025-12-16T11:28:32.647622+00:00
--- elapsed: 250ms
--- outcome: success
--- dialect: snowflake
--- node_id: not available
--- query_id: 01c11670-0307-491e-0000-001a20dfcb4d
--- desc: not provided
-select 1 as id;
